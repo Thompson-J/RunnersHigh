652744130649422c7988991a34fd5f6533e09da2
Josh <hello@josh-thompson.com>
2018-10-02 08:37:12 +0100

    1.2.2
    Fixed missing Moment.js script in '/record-activity/'

--- dev/record-activity/record-activity.php
+++ dev/record-activity/record-activity.php
@@ -36,7 +36,8 @@ include $_SERVER['DOCUMENT_ROOT'] . '/includes/head.php';
 
 <?php
 
-$addscripts = "<link rel='stylesheet' type='text/css' href='" . HOMEURL . "tomtom/map.css'/>
+$addscripts = "<script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.19.3/moment-with-locales.min.js'></script>
+	<link rel='stylesheet' type='text/css' href='" . HOMEURL . "tomtom/map.css'/>
   <script src='" . HOMEURL . "tomtom/tomtom.min.js'></script>
 	<script type='text/javascript' src='" . HOMEURL . "js/record-activity.js'></script>";
 
