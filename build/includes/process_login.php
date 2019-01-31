44c8496ba5a615164073b1c0fd4c5fe89d991b55
Josh <hello@josh-thompson.com>
2019-01-31 21:28:53 +0000

    1.2.6

--- build/includes/process_login.php
+++ build/includes/process_login.php
@@ -1,5 +1,5 @@
 <?php
-include_once 'functions.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/includes/functions.php';
  
 sec_session_start(); // Our custom secure way of starting a PHP session.
 
